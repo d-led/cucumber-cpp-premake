c83496e040bcd1b574fd677e4be75194eef7db88
Dmitry Ledentsov <dmlled@yahoo.com>
2015-01-28 15:53:17 +0100

    modularize for submodule use

--- recipes/cucumber-cpp-steps.lua
+++ recipes/cucumber-cpp-steps.lua
@@ -1,60 +1,18 @@
-local make_steps = function (name,files_,folder_)
-	local l = {
-		"cucumber-cpp-main",
-		"cucumber-cpp"
-	}
-	make_console_app(name,files_)
-		configuration { "linux" }
-		links( l )
-		links {
-			"boost_system",
-			"boost_regex",
-			"boost_chrono",
-			"boost_thread"
-		}
-        configuration { "macosx" }
-       	links( l )
-       	links {
-			"boost_system-mt",
-			"boost_regex-mt",
-			"boost_chrono-mt",
-			"boost_thread-mt"
-		}
-		configuration { "vs*" }
-		links( l )
-		configuration { "*" }
-end
+package.path=package.path..';recipes/?.lua'
 
-local make_cppspec_steps = function (name,files_,folder_)
-	make_steps(name,files_,folder_)
-	links {
-		"cucumber-cpp",
-		"cppspec",
-		"cucumber-cpp-cppspec-driver"
-	}
-end
+local steps = assert( require 'cucumber-steps' )
 
-local make_gtest_steps = function (name,files_,folder_)
-	make_steps(name,files_,folder_)
-	gmock_config()
-	links {
-		"cucumber-cpp",
-		"googlemock",
-		"cucumber-cpp-gtest-driver"
-	}
-end
-
-make_gtest_steps('TagSteps',
+steps.make_gtest_steps('TagSteps',
 	{'./cucumber-cpp/examples/FeatureShowcase/features/step_definitions/TagSteps.cpp'},
 	'./cucumber-cpp/examples/FeatureShowcase/features/step_definitions'
 )
 
-make_gtest_steps('TableSteps',
+steps.make_gtest_steps('TableSteps',
 	{'./cucumber-cpp/examples/FeatureShowcase/features/step_definitions/TableSteps.cpp'},
 	'./cucumber-cpp/examples/FeatureShowcase/features/step_definitions'
 )
 
-make_gtest_steps('GTestCalculatorSteps',
+steps.make_gtest_steps('GTestCalculatorSteps',
 	{	
 		'./cucumber-cpp/examples/Calc/features/step_definitions/GTestCalculatorSteps.cpp',
 		'./cucumber-cpp/examples/Calc/src/Calculator.cpp'
@@ -65,7 +23,7 @@ make_gtest_steps('GTestCalculatorSteps',
 		'./cucumber-cpp/examples/Calc/src',
 	}
 
-make_cppspec_steps('CppSpecCalculatorSteps',
+steps.make_cppspec_steps('CppSpecCalculatorSteps',
 	{	
 		'./cucumber-cpp/examples/Calc/features/step_definitions/CppSpecCalculatorSteps.cpp',
 		'./cucumber-cpp/examples/Calc/src/Calculator.cpp'
