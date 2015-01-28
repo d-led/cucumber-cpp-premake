5ad20a1ff458783b0d057e75cc46151d1b985135
Dmitry Ledentsov <dmlled@yahoo.com>
2015-01-28 10:44:05 +0100

    remove premake

--- premake/test-action.lua
+++
@@ -1,9 +0,0 @@
-package.path=package.path..[[;./?.lua;./?/?.lua]]
-util = assert( require 'premake.util' )
-util.start_test_of( "cppspec-test" )
-util.start_test_of( "gmock-test" )
-util.start_test_of( "gtest-test" )
-util.start_test_of( "cucumber-cpp-unit-test" )
-util.start_test_of( "cucumber-cpp-integration-test-1" )
-util.start_test_of( "cucumber-cpp-integration-test-2" )
-util.start_test_of( "cucumber-cpp-integration-test-3" )
